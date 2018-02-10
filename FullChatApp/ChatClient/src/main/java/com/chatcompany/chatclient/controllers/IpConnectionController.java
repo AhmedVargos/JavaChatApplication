270bf677a7f37cf2bbd0f87af85f6192d2677209
shimaa <khairy>
2018-02-10 16:40:59 +0200

    IpConnection

--- FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/controllers/IpConnectionController.java
+++ FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/controllers/IpConnectionController.java
@@ -5,14 +5,20 @@ package com.chatcompany.chatclient.controllers;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -21,7 +27,6 @@ import javafx.stage.Stage;
  *
  * @author Dell
  */
-
 public class IpConnectionController implements Initializable {
 
     /**
@@ -31,24 +36,41 @@ public class IpConnectionController implements Initializable {
     private Label exit;
     @FXML
     private Label min;
+    @FXML
+    private TextField ipTextfield;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-  
-    exit.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+        exit.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
                 Platform.exit();
             }
         });
-     min.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        min.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-                
-                ((Stage)min.getScene().getWindow()).setIconified(true);
+
+                ((Stage) min.getScene().getWindow()).setIconified(true);
             }
         });
-    }    
-    
+
+    }
+
+    public void connection() {
+        String ip = ipTextfield.getText();
+        String localHost="10.245.3.238";
+        Pattern IP_PATTERN = Pattern.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+   
+        if(ip.equals(localHost)&&IP_PATTERN.matcher(ip).matches()){
+            try {
+                Parent parent=FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+                        } catch (IOException ex) {
+                Logger.getLogger(IpConnectionController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
 }
