091873854af1511640c76b7641b21849ed16b53b
ahmed ehab <ahmed_ehab707@yahoo.com>
2018-02-10 13:39:15 +0200

    adding common interfaces and models

---
+++ FullChatApp/ChatServer/src/main/java/com/chatcompany/chatserver/common/LoginInterface.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chatcompany.chatserver.common;
+
+import com.chatcompany.chatserver.models.User;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author shalaby
+ */
+public interface LoginInterface extends Remote {
+
+    /**
+     * used to login
+     * @param email user email address
+     * @param pass user password
+     * @return user bean if succeed ,null if user Already signed IN
+     * @throws ResultNotFoundException If mail not found in DB
+     * @throws exceptions.WrongPasswordException if wrong password
+     * @throws RemoteException if RMI failed
+     */
+    public User login(String userName, String pass) throws RemoteException;
+
+     /**
+     * used to sign up
+     *
+     * @param user user Data
+     * @return true if succeed
+     * @throws SQLException if DB failed
+     * @throws RemoteException if RMI failed
+     *
+     */
+    public Boolean SignUp(User user) throws SQLException, RemoteException;
+
+}
