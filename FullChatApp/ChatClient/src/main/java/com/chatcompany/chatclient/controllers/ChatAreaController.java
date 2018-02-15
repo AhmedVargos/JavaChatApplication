0c7530abe86f262a847da25999fe6366af872b41
shimaa <khairy>
2018-02-15 21:55:36 +0200

    Begin Sending and receiving

--- FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/controllers/ChatAreaController.java
+++ FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/controllers/ChatAreaController.java
@@ -7,14 +7,21 @@ package com.chatcompany.chatclient.controllers;
 
 import com.chatcompany.commonfiles.commModels.User;
 import com.jfoenix.controls.JFXTabPane;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -25,7 +32,6 @@ public class ChatAreaController implements Initializable {
     @FXML
     private JFXTabPane chatTabPane;
 
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -34,6 +40,27 @@ public class ChatAreaController implements Initializable {
     public void openNewChatSession(User user) {
         //add the chat FXML box and pass the chat session obj to the chat tab controller 
         //and when send each time send message and a chat session obj
-        chatTabPane.getTabs().add(new Tab(user.getUsername()));
+
+        //Tab tab=new Tab(user.getUsername());
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/ChatBox.fxml"));
+            Tab tab = new Tab(user.getUsername());
+            tab.setContent(root);
+            Image imag=new Image("/images/x.png");
+            ImageView imgView=new ImageView(imag);
+            imgView.setFitHeight(20);
+            imgView.setFitWidth(20);
+          //  tab.setGraphic(imgView);
+            chatTabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.SELECTED_TAB);
+            chatTabPane.getTabs().add(tab);
+        } catch (IOException ex) {
+            Logger.getLogger(ChatAreaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+// Parent root = (Parent) fXMLLoader.load(this.getClass().getResource();
+        //          newValue.setContent(root);
+      
+       // chatBox
+       
+        
     }
 }
