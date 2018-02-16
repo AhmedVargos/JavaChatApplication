06e6d974ce5212bab00b1f57003591d983457d5c
EmanHesham02 <emanhesham02@gmail.com>
2018-02-16 18:05:06 +0200

    working on the chat still under work

--- FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/controllers/ChatBoxController.java
+++ FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/controllers/ChatBoxController.java
@@ -5,10 +5,17 @@ package com.chatcompany.chatclient.controllers;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import com.chatcompany.chatclient.views.MainApp;
+import com.chatcompany.commonfiles.commModels.ChatSession;
+import com.chatcompany.commonfiles.commModels.Constants;
 import com.chatcompany.commonfiles.commModels.Message;
+import com.chatcompany.commonfiles.common.ChatInterface;
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -31,6 +38,8 @@ import static javafx.scene.text.Font.font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javax.xml.datatype.XMLGregorianCalendar;
+import com.chatcompany.commonfiles.commModels.ChatSession;
+import javafx.scene.paint.Paint;
 
 /**
  * FXML Controller class
@@ -64,56 +73,58 @@ public class ChatBoxController implements Initializable {
     @FXML
     int size = 14;
     String fontf = "Arial";
-    String hex2="#000000";
-    String style ="normal";
-    String weight="normal";
+    String hex2 = "#000000";
+    String style = "normal";
+    String weight = "normal";
+    ChatSession chatSession;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO 
         List<java.lang.String> font = Font.getFamilies();
         fontFamily.getItems().addAll(font);
         fontFamily.getSelectionModel().select(2);
-        ObservableList<String> fontSizes = FXCollections.observableArrayList("8", "10", "12", "14", "18", "24","26");
+        ObservableList<String> fontSizes = FXCollections.observableArrayList("8", "10", "12", "14", "18", "24", "26");
         fontSize.setItems(fontSizes);
         fontSize.getSelectionModel().select(2);
         colorPicker.setValue(Color.BLACK);
-       
+
         colorPicker.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 messageColor = colorPicker.getValue();
                 hex2 = "#" + Integer.toHexString(messageColor.hashCode());
-               sendTextFiled.setStyle("-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
-             }
+                sendTextFiled.setStyle("-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+            }
         });
         fontSize.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 size = Integer.parseInt((String) fontSize.getSelectionModel().getSelectedItem());
-                sendTextFiled.setStyle("-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+                sendTextFiled.setStyle("-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
             }
         });
         fontFamily.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 fontf = (String) fontFamily.getSelectionModel().getSelectedItem();
-                sendTextFiled.setStyle("-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
-           }
+                sendTextFiled.setStyle("-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+            }
         });
         BoldToggleButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
 
                 weight = (BoldToggleButton.isSelected()) ? "Bold" : "normal";
-               sendTextFiled.setStyle("-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+                sendTextFiled.setStyle("-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
             }
         });
         ItalicToggleButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
                 style = (ItalicToggleButton.isSelected()) ? "italic" : "normal";
-               sendTextFiled.setStyle("-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
-           
+                sendTextFiled.setStyle("-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+
             }
         });
         UnderlineToggleButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -121,34 +132,68 @@ public class ChatBoxController implements Initializable {
             public void handle(MouseEvent e) {
                 Boolean underline = UnderlineToggleButton.isSelected();
                 if (underline) {
-                   sendTextFiled.setStyle("-fx-underline:true;-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
-           
+                    sendTextFiled.setStyle("-fx-underline:true;-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+
                 } else {
-                
-                   sendTextFiled.setStyle("-fx-underline:false;-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
-           
+
+                    sendTextFiled.setStyle("-fx-underline:false;-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+
                 }
 
             }
         });
-        
+
         ///////////// send message
         sendButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-               ///send message here 
-               String text=sendTextFiled.getText().toString();
-               if(!text.equals(null)){
-                   Label outText=new Label(text);
-                   outText.setTextFill(messageColor);
-                   outText.setStyle("-fx-font-size:"+size+";-fx-font-family:"+fontf+";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
-                   Message message=new Message(size,"String from","String to",null,hex2, fontf,style,text, weight,false);
-               ///send message to reciever
-                    HBox myHBox=new HBox();
+                ///send message here 
+                String text = sendTextFiled.getText().toString();
+                if (!text.equals(null)) {
+                    /* Label outText = new Label(text);
+                    outText.setTextFill(messageColor);
+                    outText.setStyle("-fx-font-size:" + size + ";-fx-font-family:" + fontf + ";-fx-text-inner-color:" + hex2 + ";-fx-font-style:" + style + ";-fx-font-weight:" + weight + ";");
+                    Message message = new Message(size, "String from", "String to", null, hex2, fontf, style, text, weight, false);
+                    ///send message to reciever
+                    HBox myHBox = new HBox();
                     myHBox.getChildren().add(outText);
-                   chatVBox.getChildren().add(outText);
-                   
-               }
-    }});
+                    chatVBox.getChildren().add(outText);
+                     */
+                    Message message = new Message(size, "String from", "String to", null, hex2, fontf, style, text, weight, false);
+
+                    ChatInterface chatInterface;
+                    try {
+                        chatInterface = (ChatInterface) MainApp.getServiceLoaderInterface().getServiceInstance(Constants.CHAT_SERVICE);
+
+                        chatInterface.sendMessage(message, chatSession);
+                    } catch (RemoteException ex) {
+                        Logger.getLogger(ChatBoxController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                }
+            }
+        });
+    }
+
+    public void setChatSession(ChatSession chatSession) {
+        this.chatSession = chatSession;
+
+    }
+
+    void appendText(Message msg) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                HBox myHBox = new HBox();
+                Label outText = new Label(msg.getBody());
+                //(int fontsSize, String from, String to, XMLGregorianCalendar date, String fontColor, String fontFamily, String fontStyle, String body, String fontWeight, Boolean underline) {
+
+                outText.setTextFill(Color.web(msg.getFontColor()));
+                outText.setStyle("-fx-font-size:" + msg.getFontsSize() + ";-fx-font-family:" + msg.getFontFamily() + ";-fx-text-inner-color:" + msg.getFontColor() + ";-fx-font-style:" + msg.getFontStyle() + ";-fx-font-weight:" + msg.getFontWeight() + ";");
+                myHBox.getChildren().add(outText);
+                chatVBox.getChildren().add(outText);
+
+            }
+        });
     }
 }
--- FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/models/ClientIntImp.java
+++ FullChatApp/ChatClient/src/main/java/com/chatcompany/chatclient/models/ClientIntImp.java
@@ -1,7 +1,9 @@
 package com.chatcompany.chatclient.models;
 
-
+import com.chatcompany.chatclient.controllers.ChatAreaController;
+import com.chatcompany.chatclient.controllers.ChatBoxController;
 import com.chatcompany.chatclient.controllers.ContactTabViewController;
+import com.chatcompany.chatclient.views.MainApp;
 import com.chatcompany.commonfiles.commModels.ChatSession;
 import com.chatcompany.commonfiles.common.ClientInterface;
 import java.rmi.RemoteException;
@@ -9,8 +11,11 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import com.chatcompany.commonfiles.commModels.Message;
 import com.chatcompany.commonfiles.commModels.User;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Tab;
 
 public class ClientIntImp extends UnicastRemoteObject implements ClientInterface {
+
     private ContactTabViewController mContactTabViewController;
 
     public ClientIntImp() throws RemoteException {
@@ -22,7 +27,20 @@ public class ClientIntImp extends UnicastRemoteObject implements ClientInterface
 
     @Override
     public void receiveMessage(Message msg, ChatSession chat) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        String id;
+
+        ObservableList<Tab> tabs = (ObservableList<Tab>) MainApp.getMainChatParentController().getEmbeddedChatTabsViewController().getChatTabPane().getTabs();
+        for (int i = 0; i < tabs.size(); i++) {
+            if (tabs.get(i).getId().equals(chat.getId())) {
+                ChatAreaController.append(msg, tabs.get(i).getId());
+            } else {
+                //create tab session 
+                MainApp.getMainChatParentController().getEmbeddedChatTabsViewController().openNewChatTabReciver(chat);
+                ChatAreaController.append(msg, tabs.get(i).getId());
+                
+            }
+        }
     }
 
     @Override
@@ -34,5 +52,5 @@ public class ClientIntImp extends UnicastRemoteObject implements ClientInterface
     public void updateContactsList(ArrayList<User> friend) throws RemoteException {
         mContactTabViewController.addNewFriend(friend);
     }
-   
+
 }
